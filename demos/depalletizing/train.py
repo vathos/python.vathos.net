cbe786ab31f335449668e4fd5315f72be4a3f9d3
Jonathan Balzer <jonathan.balzer@vathos.net>
2023-02-27 17:03:10 +0100

    Added a depallezing demo.

--- demos/depalletizing/train.py
+++ demos/depalletizing/train.py
@@ -10,11 +10,28 @@
 
 import os
 
+import numpy as np
+
 from vathos.authentication import get_service_account_token
-from vathos.products import get_product
+from vathos.products import create_product
+from vathos.services.depalletizing import train_product
 
 if __name__ == '__main__':
+  # authenticate with the API
   token = get_service_account_token(os.environ.get('CLIENT_ID'),
                                     os.environ.get('CLIENT_SECRET'))
-  
-  print(get_product('63f375eed312240012f01925', token))
+
+  # define camera parameters
+  projection_matrix = np.array(
+      [[1779.80041, 0, 986.39929], [0, 1782.1018, 986.39929], [0, 0, 1]],
+      dtype='f')
+  image_size = (1944, 1200)
+  image_range = (0.1, 2.0)  # in meters
+
+  # create a product
+  product = create_product('Test product 2',
+                           './demos/depalletizing/cad_model.obj', 'mm',
+                           projection_matrix, image_size, image_range, token)
+
+  # start training
+  train_product(product['_id'], './demos/depalletizing/calibration.png', token)
